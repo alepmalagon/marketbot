ec26a33921665280c9bd2136a8a164289b74fdb8
danielpmalagon <alepmalagon@gmail.com>
2025-03-27 00:05:05 -0400

    fixing some type id's

--- config.py
+++ config.py
@@ -97,38 +97,38 @@ MARAUDER_TYPE_IDS = [
 
 # Faction Battleship type IDs
 FACTION_BATTLESHIP_TYPE_IDS = [
-    17726,  # Armageddon Navy Issue (Amarr)
-    17736,  # Apocalypse Navy Issue (Amarr)
-    32305,  # Praxis (SoCT)
-    17738,  # Raven Navy Issue (Caldari)
-    17740,  # Scorpion Navy Issue (Caldari)
-    17732,  # Dominix Navy Issue (Gallente)
+    32305,  # Armageddon Navy Issue (Amarr)
+    17726,  # Apocalypse Navy Issue (Amarr)
+    47466,  # Praxis (SoCT)
+    17636,  # Raven Navy Issue (Caldari)
+    32309,  # Scorpion Navy Issue (Caldari)
+    32307,  # Dominix Navy Issue (Gallente)
     17728,  # Megathron Navy Issue (Gallente)
-    17722,  # Typhoon Fleet Issue (Minmatar)
-    17734,  # Tempest Fleet Issue (Minmatar)
+    32311,  # Typhoon Fleet Issue (Minmatar)
+    17732,  # Tempest Fleet Issue (Minmatar)
 ]
 
 # Pirate Faction Battleship type IDs
 PIRATE_BATTLESHIP_TYPE_IDS = [
     33820,  # Barghest (Guristas/Caldari)
-    17918,  # Bhaalgorn (Blood Raiders/Amarr)
-    17920,  # Nightmare (Sansha/Amarr)
-    17922,  # Rattlesnake (Guristas/Caldari)
-    17924,  # Machariel (Angel Cartel/Minmatar)
-    17926,  # Vindicator (Serpentis/Gallente)
-    33553,  # Nestor (Sisters of EVE)
+    17920,  # Bhaalgorn (Blood Raiders/Amarr)
+    17736,  # Nightmare (Sansha/Amarr)
+    17918,  # Rattlesnake (Guristas/Caldari)
+    17738,  # Machariel (Angel Cartel/Minmatar)
+    17740,  # Vindicator (Serpentis/Gallente)
+    33472,  # Nestor (Sisters of EVE)
 ]
 
 # Command Ship type IDs
 COMMAND_SHIP_TYPE_IDS = [
-    22442,  # Absolution (Amarr)
+    22448,  # Absolution (Amarr)
     22474,  # Damnation (Amarr)
-    22448,  # Nighthawk (Caldari)
-    22470,  # Vulture (Caldari)
+    22470,  # Nighthawk (Caldari)
+    22446,  # Vulture (Caldari)
     22466,  # Astarte (Gallente)
-    22464,  # Eos (Gallente)
-    22460,  # Claymore (Minmatar)
-    22456,  # Sleipnir (Minmatar)
+    22442,  # Eos (Gallente)
+    22468,  # Claymore (Minmatar)
+    22444,  # Sleipnir (Minmatar)
 ]
 
 # Strategic Cruiser type IDs
@@ -141,26 +141,26 @@ STRATEGIC_CRUISER_TYPE_IDS = [
 
 # Heavy Assault Cruiser type IDs
 HAC_TYPE_IDS = [
-    12017,  # Zealot (Amarr)
+    12003,  # Zealot (Amarr)
     12019,  # Sacrilege (Amarr)
     12011,  # Eagle (Caldari)
-    12013,  # Cerberus (Caldari)
+    11993,  # Cerberus (Caldari)
     12023,  # Deimos (Gallente)
-    12021,  # Ishtar (Gallente)
-    12015,  # Vagabond (Minmatar)
-    12025,  # Muninn (Minmatar)
+    12005,  # Ishtar (Gallente)
+    11999,  # Vagabond (Minmatar)
+    12015,  # Muninn (Minmatar)
 ]
 
 # Recon Ship type IDs
 RECON_SHIP_TYPE_IDS = [
     11965,  # Pilgrim (Amarr)
-    11957,  # Curse (Amarr)
-    11963,  # Falcon (Caldari)
-    11961,  # Rook (Caldari)
-    11971,  # Arazu (Gallente)
-    11969,  # Lachesis (Gallente)
-    11959,  # Huginn (Minmatar)
-    11967,  # Rapier (Minmatar)
+    20125,  # Curse (Amarr)
+    11957,  # Falcon (Caldari)
+    11959,  # Rook (Caldari)
+    11969,  # Arazu (Gallente)
+    11971,  # Lachesis (Gallente)
+    11961,  # Huginn (Minmatar)
+    11963,  # Rapier (Minmatar)
 ]
 
 # Combined list of all battleship type IDs
